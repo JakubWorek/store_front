c351b302fd348d652ea80167fd2dac71f8e94c01
Jakub Worek <worekkuba@gmail.com>
2023-10-29 18:45:01 +0100

    cart checkout

--- pages/cart.js
+++ pages/cart.js
@@ -51,7 +51,7 @@ const CityHolder = styled.div`
 `;
 
 export default function CartPage(){
-  const {cartProducts, addProduct, removeProduct} = useContext(CartContext);
+  const {cartProducts, addProduct, removeProduct, clearCart} = useContext(CartContext);
   const [products, setProducts] = useState([]);
 
   const [name,setName] = useState('');
@@ -60,6 +60,7 @@ export default function CartPage(){
   const [postalCode,setPostalCode] = useState('');
   const [streetAddress,setStreetAddress] = useState('');
   const [country,setCountry] = useState('');
+  const [isSuccess,setIsSuccess] = useState(false);
 
   useEffect(() => {
     if(cartProducts.length > 0){
@@ -71,6 +72,16 @@ export default function CartPage(){
       setProducts([]);
     }
   }, [cartProducts]);
+
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      return;
+    }
+    if (window?.location.href.includes('success')) {
+      setIsSuccess(true);
+      clearCart();
+    }
+  }, []);
 
   function moreOfThisProduct(id){
     addProduct(id);
@@ -101,7 +112,7 @@ export default function CartPage(){
     total += price;
   }
 
-  if(window.location.href.includes('success')){
+  if(isSuccess){
     return(
       <>
         <Header />
